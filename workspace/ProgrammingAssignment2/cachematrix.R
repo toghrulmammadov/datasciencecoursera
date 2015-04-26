5d5795140a07e9f7095bbf321f4cc751535a7868
toghrulmammadov <togrul.isa@gmail.com>
2015-04-27 00:22:50 +0300

    Programming Assignment 2

---
+++ workspace/ProgrammingAssignment2/cachematrix.R
@@ -0,0 +1,29 @@
+makeCacheMatrix <- function(x = matrix()) {
+  
+  xinv <- NULL 
+  
+  set <- function(y) {
+    x <<- y
+    xinv <<- NULL 
+  
+  get <- function() x
+  setInv <- function(inv) xinv <<- inv 
+  getInv <- function() xinv 
+
+  list(set = set, get = get,
+       setInv = setInv,
+       getInv = getInv)
+}
+
+
+cacheSolve <- function(x, ...) {
+  m <- x$getInv() 
+  if(!is.null(m)) { 
+    message("getting cached data")
+    return(m) 
+  }
+  data <- x$get() 
+  m <- solve(data) 
+  x$setInv(m) 
+  m 
+}
